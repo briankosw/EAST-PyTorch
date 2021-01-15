42584de555ea1023651c9289417734e1103164fa
Brian Ko <briankosw@gmail.com>
2021-01-15 16:45:14 +0900

    Implement ICDARDataModule

--- icdar.py
+++ icdar.py
@@ -1,18 +1,124 @@
 import csv
 import os
 from glob import glob
-from typing import Callable, Optional, Tuple, Union
+from typing import Any, Callable, Optional, Sequence, Tuple, Union
 
 import cv2
 import numpy as np
 from PIL import Image
+import pytorch_lightning as pl
 import torch
+from torch import Tensor
+from torch.utils.data import DataLoader
 from torchvision.datasets import VisionDataset
 import torchvision.transforms as transforms
 import torchvision.transforms.functional as F
 
 # Text that should not affect evaluation: https://rrc.cvc.uab.es/?ch=4&com=tasks
 DO_NOT_CARE = "###"
+
+
+class ICDARDataModule(pl.LightningDataModule):
+    def __init__(self,
+        dataset_dir: str,
+        input_shape: Union[int, Tuple[int, int]],
+        min_text_size: int,
+        batch_size: int,
+        shuffle: bool = True,
+        num_workers: int = 0,
+        pin_memory: bool = True,
+        seed: int = 0,
+        *args: Any,
+        **kwargs: Any
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self.dataset_dir = dataset_dir
+        if isinstance(input_shape, tuple):
+            self.input_shape = input_shape
+        else:
+            self.input_shape = (input_shape, input_shape)
+        self.min_text_size = min_text_size
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.num_workers = num_workers
+        self.pin_memory = pin_memory
+        self.seed = seed
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        if stage == "fit" or stage is None:
+            train_transforms = (
+                self.default_transforms()
+                if self.train_transforms is None
+                else self.train_transforms
+            )
+            val_transforms = (
+                self.default_transforms()
+                if self.val_transforms is None
+                else self.val_transforms
+            )
+            self.train_dataset = ICDARDataset(
+                self.dataset_dir,
+                input_shape=self.size,
+                min_text_size=self.min_text_size,
+                transforms=train_transforms
+            )
+            self.val_dataset = ICDARDataset(
+                self.dataset_dir,
+                input_shape=self.size,
+                min_text_size=self.min_text_size,
+                transforms=train_transforms
+            )
+        # TODO: Fix test_dataset
+        if stage == "test" or stage is None:
+            test_transforms = (
+                self.default_transforms()
+                if self.test_transforms is None
+                else self.test_transforms
+            )
+            self.test_dataset = ICDARDataset(
+                self.dataset_dir, train=False, transforms=test_transforms
+            )
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=self.shuffle,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            collate_fn=self.collate_fn
+        )
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+        )
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+        )
+
+    def default_transforms(self) -> Any:
+        return RandomCrop(self.size)
+
+    def collate_fn(self, batch):
+        images = torch.stack([b[0] for b in batch], dim=0)
+        score_maps = torch.stack([b[1] for b in batch], dim=0)
+        geometry_maps = torch.stack([b[2] for b in batch], dim=0)
+        train_ignore_masks = torch.stack([b[3] for b in batch], dim=0)
+        train_boundary_masks = torch.stack([b[4] for b in batch], dim=0)
+        return (
+            images, score_maps, geometry_maps, train_ignore_masks,train_boundary_masks
+        )
 
 
 class ICDARDataset(VisionDataset):
@@ -25,6 +131,17 @@ class ICDARDataset(VisionDataset):
         target_transform: Optional[Callable] = None,
         transforms: Optional[Callable] = None,
     ) -> None:
+        """
+        ICDAR 2015 dataset
+
+        Arguments:
+            root:
+            input_shape:
+            min_text_size:
+            transform:
+            target_transform:
+            transforms:
+        """
         super(ICDARDataset, self).__init__(root, transforms, transform, target_transform)
         image_pathname = os.path.join(root, "images/*.jpg")
         self.images = sorted(glob(image_pathname))
@@ -36,7 +153,7 @@ class ICDARDataset(VisionDataset):
             self.input_shape = (input_shape, input_shape)
         self.min_text_size = min_text_size
 
-    def __getitem__(self, idx: int):
+    def __getitem__(self, idx: int) -> Tuple[Tensor, Tensor, Tensor, Tensor, Tensor]:
         image = Image.open(self.images[idx])
         quads, texts = load_annotation(self.annotations[idx])
         # Crop
@@ -46,24 +163,49 @@ class ICDARDataset(VisionDataset):
             quads, texts, input_shape=self.input_shape, min_text_size=self.min_text_size
         )
         # Image augmentation
-        if self.transform:
-            image = self.transform(image)
-        return image, quads, texts
+        image = transforms.ToTensor()(image)
+        return image, score_map, geometry_map, train_ignore_mask, train_boundary_mask
 
     def __len__(self) -> int:
         return len(self.images)
 
 
 class RandomCrop(transforms.RandomCrop):
-    def __init__(self, size, padding=None, pad_if_needed=False, fill=0, padding_mode="constant"):
+    def __init__(
+        self,
+        size: Tuple[int, int],
+        padding: Optional[Union[int, Sequence[int]]] = None,
+        pad_if_needed: bool = False,
+        fill: Union[int, Tuple[int, int, int]] = 0,
+        padding_mode: str = "constant"
+    ) -> None:
+        """
+        Crops the image at a random location. Quadrilaterals and texts are cropped out
+        if the quadrilateral is not fully inside the cropped region.
+
+        Arguments:
+            size:
+            padding:
+            pad_if_needed:
+            fill:
+            padding_mode:
+        """
         super().__init__(size, padding, pad_if_needed, fill, padding_mode)
 
-    def forward(self, image, quads, texts):
+    def forward(
+        self,
+        image: Union[Image.Image, Tensor],
+        quads: np.ndarray,
+        texts: np.ndarray
+    ) -> Tuple[Union[Image.Image], np.ndarray, np.ndarray]:
         """
         Args:
-            img (PIL Image or Tensor): Image to be cropped.
+            image: image to be cropped
+            quads:
+            texts:
+
         Returns:
-            PIL Image or Tensor: Cropped image.
+
         """
         if self.padding is not None:
             image = F.pad(image, self.padding, self.fill, self.padding_mode)
@@ -84,6 +226,7 @@ class RandomCrop(transforms.RandomCrop):
             quads[i] = sort_vertices(quads[i])
             quad_left, quad_top = quads[i][0]
             quad_right, quad_bottom = quads[i][2]
+            # Only quadrilaterals that are fully inside the cropped image is included
             if (
                 left < quad_left and top < quad_top and quad_right < right and quad_bottom < bottom
             ):
@@ -133,7 +276,7 @@ def generate_maps(
     texts: np.ndarray,
     input_shape: Tuple[int, int],
     min_text_size: int,
-) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
     """
     Generates the score map
 
@@ -165,7 +308,7 @@ def generate_maps(
         shrinked_quad = shrink_quad(quad.copy(), ref_len)
         cv2.fillPoly(score_map, shrinked_quad, 1)
         cv2.fillPoly(original_quad_mask, quad[np.newaxis, ...].astype(np.int32), 1)
-        cv2.fillPoly(score_map, shrinked_quad, i + 1)
+        cv2.fillPoly(shrinked_quad_mask, shrinked_quad, i + 1)
         shrinked_quad_points = np.argwhere(shrinked_quad_mask == i + 1)
         dists = np.linalg.norm(quad - np.roll(quad, -1, axis=0), axis=1)
         top, right, bottom, left = dists
@@ -242,12 +385,12 @@ def minimum_bounding_rectangle(quad: np.ndarray) -> Tuple[np.ndarray, float]:
 
 
 def sort_vertices(quad: np.ndarray) -> np.ndarray:
-        # Calculate the centroid and use the centroid to determine left and right
+    # Calculate the centroid and use the centroid to determine left and right
     # vertices. For each side, compare the y value to determine top and bottom.
     center = np.mean(quad, axis=0)
-    left = np.argwhere(quad[:, 0] < center[0]).squeeze()
+    left = np.argwhere(quad[:, 0] < center[0]).reshape(-1)
     tl, bl = left[np.argsort(quad[left, 1])]
-    right = np.argwhere(quad[:, 0] > center[0]).squeeze()
+    right = np.argwhere(quad[:, 0] > center[0]).reshape(-1)
     tr, br = right[np.argsort(quad[right, 1])]
     quad = quad[[tl, tr, br, bl]]
     return quad
